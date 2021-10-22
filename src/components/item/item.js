fdfa93e6f03ccc6a326ecd8bd8069e7da28ed7e3
Gabriel Sarmiento Lamas <sarmiento.lamas.g.a@gmail.com>
2021-10-21 22:47:50 -0300

    Cambios en css

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -6,11 +6,15 @@ import ItemCount from "../itemCount/itemCount";
 function Item(producto) {
   return (
     <div key={producto.id} className="productCard">
-      <p className="product">{producto.name}</p>
+      <p className="productName">{producto.name}</p>
       <div className="imgContainer">
-        <img className="imgProduct" src={producto.photo} alt="IMAGEN"></img>
-        <p className="price">{producto.price}</p>
+        <img
+          className="imgProduct"
+          src={producto.photo}
+          alt={producto.name}
+        ></img>
       </div>
+      <p className="price">{producto.price}</p>
       <div className="productInteractive">
         <ItemCount initial={1} stock={5} />
       </div>
