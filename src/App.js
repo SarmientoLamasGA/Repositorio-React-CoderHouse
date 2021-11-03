41b923140980bc1ec39252b889403566fdc253ea
Gabriel Sarmiento Lamas <sarmiento.lamas.g.a@gmail.com>
2021-11-03 19:32:08 -0300

    Modificados objetos del arrray

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import "./App.css";
 import Navbar from "./components/navbar/navbar";
 import ItemListContainer from "./components/itemListContainer/itemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <header className="App-header">
-        <Navbar />
-      </header>
-      <main>
-        <ItemListContainer titulo="Cartilla de productos" />
-      </main>
-      <section>
-        <ItemDetailContainer />
-      </section>
+      <BrowserRouter>
+        <header className="App-header">
+          <Navbar />
+        </header>
+        <Switch>
+          <Route exact path="/">
+            <>
+              <ItemListContainer titulo="Cartilla de productos" />
+            </>
+            <>
+              <ItemDetailContainer />
+            </>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
