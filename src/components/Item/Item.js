9bd7d76edbf724659e7ac761d5a95850fc85a5b0
Gabriel Sarmiento Lamas <sarmiento.lamas.g.a@gmail.com>
2021-11-30 20:11:58 -0300

    Eliminados botones en Item

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -6,13 +6,13 @@ import "./Item.scss";
 function Item({ id, name, photo, price }) {
   return (
     <div key={id} className="productCard">
-      <div className="imgContainer">
-        <img className="imgProduct" src={photo} alt={name}></img>
-      </div>
-      <p className="productName">{name}</p>
-      <p className="price">${price}</p>
-      <Link to={`/producto/${id}`}>
-        <button className="seeMore">Ver Detalles</button>
+      <Link className="cardLink" to={`/producto/${id}`}>
+        <div className="imgContainer">
+          <img className="imgProduct" src={photo} alt={name}></img>
+        </div>
+        <p className="productName">{name}</p>
+        <p className="price">${price}</p>
+        <p className="seeMore">Click para ver Detalles</p>
       </Link>
     </div>
   );
