10d8291fb8b6b9a55854a3214dc3c5f11a0d9225
Gabriel Sarmiento Lamas <sarmiento.lamas.g.a@gmail.com>
2021-12-07 20:52:22 -0300

    Modificado estilo Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
             LEGUMBRES
           </NavLink>
         </li>
-        <Link to={`/Cart`}>
+        <Link to={`/Cart`} activeClassName="active">
           <CartWidget />
         </Link>
       </ul>
